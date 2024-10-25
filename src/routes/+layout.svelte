26f54853ead89da34a7d07512be1f027635623f1
rrivera <rrivera@redbannermedia.com>
2024-10-25 18:36:10 -0400

    updates to scaffold this site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,22 @@
 <script>
-  import "../app.postcss";
-  import {DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte";
-  import {page} from "$app/stores";
-  import {base} from "$app/paths";
-  import {GithubSolid} from "flowbite-svelte-icons";
-  import ReusableModal from "$lib/components/ReusableModal.svelte";
+    import "../app.postcss";
+    import {Footer, FooterCopyright, DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte";
+    import ReusableModal from "$lib/components/ReusableModal.svelte";
 </script>
-<Navbar class="z-10" let:hidden let:toggle>
-    <NavBrand>
-        <DarkMode/>
-        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-3">
-      <a href="{base}/">Svelte kit / Flowbite - starter </a>
-    </span>
-    </NavBrand>
-    <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
-        <NavLi href="{base}/docs" active={$page.url.pathname.endsWith('docs')}>DOCS</NavLi>
-        <NavLi href="https://github.com/sroehrl/svelte-flowbite-boilerplate#readme" >
-            <GithubSolid/>
-        </NavLi>
-    </NavUl>
-</Navbar>
-<div class="my-5">
-<slot />
+<div class="py-2 px-4">
+    <Navbar class="z-10 w-full rounded bg-primary-600 border-2 border-[#929292]" let:hidden let:toggle>
+        <NavBrand>
+            Test
+        </NavBrand>
+        <NavHamburger on:click={toggle}/>
+        <NavUl {hidden}>
+        </NavUl>
+    </Navbar>
+    <slot/>
+    <Footer class="fixed inset-x-0 bottom-0 pb-2">
+        <div class="w-6/12 h-16 rounded mx-auto bg-yellow-400">
+            Test
+        </div>
+    </Footer>
     <ReusableModal/>
 </div>
