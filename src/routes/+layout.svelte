e51b86aa599e1677db33a7a372336c902d44277f
rrivera <rrivera@redbannermedia.com>
2024-10-26 14:46:32 -0400

    Fixed layout coloring and design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,32 @@
-<script>
+<script lang="ts">
     import "../app.postcss";
-    import {Footer, FooterCopyright, DarkMode, Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte";
+    import {Button, Footer, Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte";
     import ReusableModal from "$lib/components/ReusableModal.svelte";
+    import {ClockOutline} from "flowbite-svelte-icons";
 </script>
-<div class="py-2 px-4">
-    <Navbar class="z-10 w-full rounded bg-primary-600 border-2 border-[#929292]" let:hidden let:toggle>
-        <NavBrand>
-            Test
+<div>
+    <Navbar color="transparent" class="z-10 w-full" let:hidden let:toggle>
+        <NavBrand href="/">
+            <ClockOutline class="w-12 h-12 fill-red-600 mr-4"/>
+            <span class="text-white text-3xl font-bold">Svelte Time</span>
         </NavBrand>
         <NavHamburger on:click={toggle}/>
         <NavUl {hidden}>
+            <NavLi href="/" on:click={toggle}>
+                <Button color="blue" class="text-white">
+                    Current Tasks
+                </Button>
+            </NavLi>
+            <NavLi href="/completed" on:click={toggle}>
+                <Button color="green" class="text-white">
+                    Completed
+                </Button>
+            </NavLi>
         </NavUl>
     </Navbar>
     <slot/>
     <Footer class="fixed inset-x-0 bottom-0 pb-2">
-        <div class="w-6/12 h-16 rounded mx-auto bg-yellow-400">
+        <div class="w-5/12 h-16 rounded mx-auto bg-red-600 p-1">
             Test
         </div>
     </Footer>
