4843b969f01057a5bd84f52348dcaaf87fa80b06
rrivera <rrivera@redbannermedia.com>
2024-10-31 03:07:58 -0400

    Cleaned up styling

--- src/lib/components/ToDoCard.svelte
+++ src/lib/components/ToDoCard.svelte
@@ -31,17 +31,12 @@
             completed: !completed
         });
     };
+    $: completedTaskBorder = completed ? 'bg-green-900 border-green-500' : 'bg-slate-800 border-gray-500';
+    $: completedTaskText = completed ? 'text-green-300 border-green-500' : 'text-white border-gray-500';
+    $: completedTaskIcon = completed ? 'text-green-300' : 'text-gray-300';
 </script>
-<div class={
-            completed
-            ? 'flex flex-row justify-between w-full bg-green-900 pr-2 border-2 border-green-500 rounded my-4'
-            : 'flex flex-row justify-between w-full bg-slate-800 pr-2 border-2 border-gray-500 rounded my-4'
-            }>
-    <div class={
-            completed
-            ? 'text-green-300 flex flex-col border-green-500 border-l-8 py-2 pl-2 cursor-pointer'
-            : 'text-white flex flex-col border-gray-500 border-l-8 py-2 pl-2 cursor-pointer'
-            }>
+<div class="flex flex-row justify-between w-full {completedTaskBorder} pr-2 border-2 rounded my-4">
+    <div class="flex flex-col {completedTaskText} border-l-8 py-2 pl-2">
         <div class="flex flex-row justify-between w-full">
             <h4 class="text-2xl mb-3">
                 {title}
@@ -54,21 +49,14 @@
     <div class="text-white flex flex-col py-2 pr-2">
         <ul class="flex flex-row place-self-end space-x-2 mb-3">
             <li>
-                <EditOutline class="size-3 hover:text-blue-400 cursor-pointer"
-                             on:click={editModal}/>
+                <EditOutline class="size-3 hover:text-blue-400 cursor-pointer" on:click={editModal}/>
             </li>
             <li>
-                <CheckCircleOutline
-                        class={
-                        completed ?
-                        "text-green-300 size-3 hover:text-green-400 cursor-pointer"
-                        : "text-gray-300 size-3 hover:text-green-400 cursor-pointer"
-                        }
-                        on:click={completeTodo}/>
+                <CheckCircleOutline class="{completedTaskIcon} size-3 hover:text-green-400 cursor-pointer"
+                                    on:click={completeTodo}/>
             </li>
             <li>
-                <CloseOutline class="size-2 hover:text-red-300 cursor-pointer"
-                              on:click={deleteTodo}/>
+                <CloseOutline class="size-2 hover:text-red-300 cursor-pointer" on:click={deleteTodo}/>
             </li>
         </ul>
     </div>
